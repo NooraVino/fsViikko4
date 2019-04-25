bac0b59f14707b576ae4c3cb329baa78418ee4d8
Noora Virolainen <vino@cubbli18-gold.local>
2019-04-25 15:50:32 +0300

    4.10, 4.11 ja 4.12 ok

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,24 @@
 const notesRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-notesRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
-  
-  notesRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-  })
+notesRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs.map(blog => blog.toJSON()))
+})
+
+notesRouter.post('/', async (request, response) => {
+  const blog = new Blog(request.body)
+
+  if (!blog.likes) {
+    blog.likes = 0
+  }
+  if (!blog.title || !blog.url) {
+    return response.status(400).send({error: 'et voi lisätä blogia ilman otsikkoa tai osoitetta.'})
+  }
+
+  const newBlog = await blog.save()
+  response.json(newBlog.toJSON())
+
+})
 
 module.exports = notesRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,33 +1,19 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
 
-const blogs = [
-{
-  title: "kokeilu",
-  author: "ksksks",
-  url: "hhfhd",
-  likes: 2,
-  id: "5c67dd9f13152d61377d2782"
-  },
-  {
-  title: "toinen",
-  author: "nälkä",
-  url: "hhfhjddjdj",
-  likes: 100,
-  id: "5c67de4713152d61377d2783"
-  },
-]
+
 
 beforeEach(async () => {
   await Blog.deleteMany({})
 
-  let noteObject = new Blog(blogs[0])
+  let noteObject = new Blog(helper.blogs[0])
   await noteObject.save()
 
-  noteObject = new Blog(blogs[1])
+  noteObject = new Blog(helper.blogs[1])
   await noteObject.save()
 })
 
@@ -41,11 +27,87 @@ test('blogs are returned as json', async () => {
 
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
-
-  expect(response.body.length).toBe(blogs.length)
+  expect(response.body.length).toBe(helper.blogs.length)
 })
 
 
+test('a valid blog can be added ', async () => {
+  const newBlog = {
+    title: "uusi",
+    author: "noora",
+    url: "jaajajaja",
+    likes: 10,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(r => r.title)
+
+  expect(response.body.length).toBe(helper.blogs.length + 1)
+  expect(titles).toContain("uusi")
+})
+
+test('blog without likes cant add', async () => {
+  const newBlog = {
+    title: "ilmanLikeja",
+    author: "kalle",
+    url: "jjeee",
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+
+  const response = await api.get('/api/blogs')
+
+  expect(response.body.length).toBe(helper.blogs.length +1 )
+
+//  tässä pitäs löytää response-joukosta uusi lisätty blogi ja tallentaa se muuttujaklsi newOne
+
+      // expect(newOne.likes).toBe(0)
+
+})
+
+test('blog without title or url cant add', async () => {
+  const newBlog = {
+    author: "without url",
+    likes: "7"
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+})
+
+// test('a note can be deleted', async () => {
+//   const response = await api.get('/api/blogs')
+//   const blogsAtStart = response.body.map(b => b)
+
+//   const noteToDelete = blogsAtStart[0]
+
+//   await api
+//     .delete(`/api/blogs/${noteToDelete.id}`)
+//     .expect(204)
+
+//     const response1 = await api.get('/api/blogs')
+//     const notesAtEnd = response1.body.map(b => b)
+
+//   expect(notesAtEnd.length).toBe(
+//     blogs.length - 1
+//   )
+
+//   const contents = notesAtEnd.map(r => r.title)
+
+//   expect(contents).not.toContain(noteToDelete.title)
+// })
 
 
 afterAll(() => {
